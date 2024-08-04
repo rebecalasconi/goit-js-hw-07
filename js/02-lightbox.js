d9b2ee8553516c3d8f655a37b719859cd5d96192
rebecalasconi <rebecavoicilas@yahoo.com>
2024-08-05 10:47:47 +1400

    prima verificare

---
+++ js/02-lightbox.js
@@ -0,0 +1,43 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const createGalleryMarkup = (items) => {
+    return items
+    .map((item) => {
+    const { preview, original, description } = item;
+    return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+};
+
+const renderGallery = () => {
+    galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+};
+
+renderGallery();
+
+
+console.log(galleryItems);
+
+function onGallleryClick(event) {
+    event.preventDefault();
+    const isGalleryImg = event.target.classList.contains("gallery__image");
+
+    if(!isGalleryImg) return;
+
+    const imgSource = event.target.dataset.source;
+    new SimpleLightbox('.gallery a', { captionDelay: 250, captionPosition: 'bottom', captionData: 'alt' });
+
+}
+
+
+galleryContainer.addEventListener("click", onGallleryClick);
